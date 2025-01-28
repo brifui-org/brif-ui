fe09adfd1143a5fadd7ea1641fda280f50810c0d
thinh.trinhduc <thinh.trinh@persefoni.com>
2025-01-28 11:19:02 +0700

    feat: updated menu active track

--- packages/components/menu/src/track.tsx
+++ packages/components/menu/src/track.tsx
@@ -20,11 +20,14 @@ export const Track: React.FC<MenuTrackProps> = ({
     <div
       className={cn(
         [
-          "absolute top-0 left-0 bg-default/10 w-full h-full rounded-md pointer-events-none",
-          "transition-[var(--menu-track-transition-property,_all)] h-7 [transition-timing-function:cubic-bezier(0.25,0.1,0.25,1)]",
-          "translate-y-[var(--menu-vertical-top,_0px)]",
-          "opacity-[var(--menu-track-opacity,_0)]"
+          "absolute top-0 left-0 w-full h-full rounded-md pointer-events-none",
+          "h-7 [transition-timing-function:cubic-bezier(0.25,0.1,0.25,1)]"
         ],
+        {
+          ["bg-default/10 transition-[var(--hover-track-transition-property,_all)] opacity-[var(--hover-track-opacity,_0)] translate-y-[var(--hover-track-vertical-top,_0px)] -z-[10]"]:
+            triggerType === "hover",
+          ["bg-primary/90 transition-[var(--active-track-transition-property,_none)] opacity-[var(--active-track-opacity,_0)] translate-y-[var(--active-track-vertical-top,_0px)] -z-[5]"]: triggerType === "active"
+        },
         className
       )}
       {...props}
