b2708dda8ad6b2668b019e6201a1ba4bbd48d803
Thinh Trinh Duc <thinh.duc.trinh@gmail.com>
2025-02-16 07:17:08 +0700

    feat: updated

---
+++ packages/cli/dist/index.mjs
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+import {
+  main
+} from "./chunk-E6QCQHPO.mjs";
+import "./chunk-52FHM3CF.mjs";
+
+// src/index.ts
+main();
---
+++ packages/cli/dist/main.mjs
@@ -0,0 +1,7 @@
+import {
+  main
+} from "./chunk-E6QCQHPO.mjs";
+import "./chunk-52FHM3CF.mjs";
+export {
+  main
+};
